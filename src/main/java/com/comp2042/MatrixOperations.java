f119fcdce8ca16c393fa6cdf66e6ebb4eb0bac2b
An Shiqi <hcysa5@nottingham.edu.my>
2025-11-10 23:39:29 +0800

    change code in line 22 from brick [j] [i] to brick[i][j]

--- src/main/java/com/comp2042/MatrixOperations.java
+++ src/main/java/com/comp2042/MatrixOperations.java
@@ -19,7 +19,7 @@ public class MatrixOperations {
             for (int j = 0; j < brick[i].length; j++) {
                 int targetX = x + i;
                 int targetY = y + j;
-                if (brick[j][i] != 0 && (checkOutOfBound(matrix, targetX, targetY) || matrix[targetY][targetX] != 0)) {
+                if (brick[i][j]!= 0 && (checkOutOfBound(matrix, targetX, targetY) || matrix[targetY][targetX] != 0)) {
                     return true;
                 }
             }
